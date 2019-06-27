dc2c101ea7074e441df518787d2400fbde4bbd5b
Simon Chius <simon@anywhere.co>
2019-06-27 14:09:54 +0530

    Home page and Configuration page is done.

--- app/src/main/java/com/simonmisles/powerbackup/Receiver/SMSReceiver.kt
+++ app/src/main/java/com/simonmisles/powerbackup/Receiver/SMSReceiver.kt
@@ -5,12 +5,14 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import android.widget.Toast
+import com.simonmisles.powerbackup.Helper.JobScheduleHelper
 
 class SMSReceiver : BroadcastReceiver(){
     val TAG: String = "SMSReceiver"
     override fun onReceive(context: Context?, intent: Intent?) {
-        Toast.makeText(context,"New SMS Received", Toast.LENGTH_LONG).show()
+//        Toast.makeText(context,"New SMS Received", Toast.LENGTH_LONG).show()
         Log.d(TAG,"New SMS received")
+        JobScheduleHelper(context!!).schedule()
     }
 
 }
